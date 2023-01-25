e4681f12c329cf11c8db6ad90a097ec68a7ebdb2
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-01-25 18:39:59 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,61 @@
+export const item = {
+    "id": "f043b5ab-018d-4524-aaaf-0266079fee9f",
+    "composers": "spend",
+    "view_all": "care",
+    "composers_list": [
+        {
+            "id": "5740d22e-3b82-4516-b62b-eb0f97fa7e3c",
+            "composer_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/35.png",
+            "composer_name": "western"
+        },
+        {
+            "id": "6d6542ee-117a-44f7-bc22-17e6e950f7a6",
+            "composer_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/241.png",
+            "composer_name": "hold"
+        },
+        {
+            "id": "0de74626-81ba-47ee-a4ed-f5d5049b576a",
+            "composer_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/324.png",
+            "composer_name": "form"
+        },
+        {
+            "id": "35c89303-738d-47e9-966f-6dce815cc6aa",
+            "composer_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/196.png",
+            "composer_name": "should"
+        },
+        {
+            "id": "01cf5988-6488-47d9-948b-ed6272cc138c",
+            "composer_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/293.png",
+            "composer_name": "society"
+        }
+    ],
+    "ensemble": "case",
+    "view_ensembles": "store",
+    "ensemble_list": [
+        {
+            "id": "277b2d30-bbdc-4e14-8d35-876edfedec18",
+            "ensemble_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/238.png",
+            "ensemble_name": "citizen"
+        },
+        {
+            "id": "3f909d84-fec0-448f-b3bd-8c67dd11e53c",
+            "ensemble_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/295.png",
+            "ensemble_name": "sister"
+        },
+        {
+            "id": "982d3162-ef67-4233-8879-642cc7a2a068",
+            "ensemble_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/364.png",
+            "ensemble_name": "believe"
+        },
+        {
+            "id": "d57fc182-f2b7-4665-96ed-7441682c04a1",
+            "ensemble_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/239.png",
+            "ensemble_name": "health"
+        },
+        {
+            "id": "8cc56517-a557-4fe8-be3c-2fdef3a8c3c0",
+            "ensemble_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/323.png",
+            "ensemble_name": "far"
+        }
+    ]
+}
